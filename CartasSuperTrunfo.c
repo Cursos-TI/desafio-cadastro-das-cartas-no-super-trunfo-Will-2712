e8991052ca29fabe22d10d20dd19ed3d752519bf
William <wlndudu93@gmail.com>
2025-02-19 10:34:25 -0300

    CST1_SP.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,25 +1,27 @@
-//cadastro cartas
 #include <stdio.h>
 
 int main() {
-
+//Criei o arquivo char para colocar a informação"carta" essa vai ser a primeira informação e identificação de qual é a carta de n1.
     char carta = '1';
-    char estado = 'S';
-    char codigo [05] = "S01";
+//AQUI COMECEIA USAR ARRAYS
+    char estado [03] = "SP";
+    char codigo [05] = "A01";
     char cidade [20] = "São Paulo";
-    int populacao = 1145000000;
+//aqui em população usei "float" ao invés do "int" porque o float me ajuda na impressão conforme eu quero(com caracteres e ponto)assim facilitando a informação na carta "ao meu ver".
+    float populacao = 11.45;
+//daqui em diante, segui as informações dadas na aula.
     float area = 1521.11;
     float pib = 11.655;
-    int ponto = 36
-
+    int ponto = 36;
+//nas impressões, segui o padrão informado nas aulas, fui alternando cada modo (%e%d%fetc para ver como ficava, assim cheguei na conclusão de usar o float ao invés do int na população, e após a impressão acrescentei o texto conforme achei correto.
     printf("Carta:%c\n", carta);
     printf("Estado:%c\n", estado);
     printf("Código: %s\n", codigo);
     printf("Nome da Cidade:%s\n", cidade);
-    printf("População: %i\n", populacao);
+    printf("População: %i milhões de habitantes\n", populacao);
     printf("Área : %.2f km²\n", area);
     printf("PIB: %.3f trilhões de Reais\n", pib);
     printf("Número de Pontos Turísticos: %i\n, ponto");
-
+//com esse padrão, vou criar as demais cartas, alterar as informações e o que for necessário nas impressões.
     return 0;
 }
